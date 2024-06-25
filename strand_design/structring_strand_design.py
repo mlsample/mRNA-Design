df404196572f6ea8ad6f9a2d2b7fa46b17f2b3c0
mlsample <mlsample@asu.edu>
2024-06-25 18:29:31 +0200

    Decent progress on the strucutring strand design algorithm. I just need to implement the mutation and export functions.

--- strand_design/structring_strand_design.py
+++ strand_design/structring_strand_design.py
@@ -1,23 +1,53 @@
 import ipy_oxdna as iox
 import ipy_oxdna.dna_structure as dna
+from oxDNA_analysis_tools.output_bonds import output_bonds
+from oxDNA_analysis_tools.UTILS.RyeReader import get_confs, inbox, describe
+from oxDNA_analysis_tools.UTILS.oxview import oxdna_conf
 import numpy as np
 import argparse
+import subprocess
 from pathlib import Path
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='FATCAT and TM-Align Strucutral Homology Screens')
-    parser.add_argument('-s', '--strucutre_file', metavar='strucutre_file', type=str, help='Directory containing the query PDB files EX: ./query_dir/')
-    parser.add_argument('-i', '--input_md_file', metavar='input_md_file', type=str, help='Directory of proteome directories EX: ./proteomes/ (where ./proteomes contains multiple dirs called i.e human, mouse, drome)')
-    parser.add_argument('-c', '--coding_sequence', metavar='output_name', type=str, help='Location to save the csv formatted results of the FATCAT and TM-Align search. Default: ./fatcat_tmalign_homology_search.csv')
-    parser.add_argument('-o', '--output_name', metavar='output_name', type=str, help='Location to save the csv formatted results of the FATCAT and TM-Align search. Default: ./fatcat_tmalign_homology_search.csv')
-    parser.add_argument('--force_overwrite', action='store_true', help='Force overwrite of existing results if they exist.')
-    args = parser.parse_args()
-    return args, parser
+import sys
+import pandas as pd
 
 def main():
-    strucutre_file, input_md_file, coding_sequence, output_name = collect_args()
-    
+    strucutre_file, topology_file, input_md_file, coding_sequence, output_name = collect_args()
 
+    coding_seq_str = parse_coding_sequence(coding_sequence)
+
+    strucutre = get_structure(strucutre_file, topology_file)
+
+    hb_id_1, hb_id_2 = get_hblist(strucutre_file, topology_file, input_md_file)
+    
+    half, indexes_1, indexes_2 = get_halfs(strucutre)
+    
+    pair_map = make_pair_map(hb_id_1, hb_id_2)
+    index_to_seq_map = get_index_to_seq_map(strucutre)
+    
+    coding_indexes, coding_complement = get_idx_of_coding_complement(indexes_1, indexes_2, pair_map, coding_seq_str)
+    
+    coding_binds_coding_indexes = find_where_coding_binds_coding(coding_indexes, coding_complement, pair_map)
+    
+    return 0
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Embeds a coding sequence into a ssOrigami strucutre file')
+    parser.add_argument('-s', '--strucutre_file', metavar='strucutre_file', type=str,
+                        help='Path to oxDNA .dat file of ssOrigami strucutre EX: ./ssOrigami.dat')
+    parser.add_argument('-t', '--topology_file', metavar='topology_file', type=str,
+                        help='Path to oxDNA .top file of ssOrigami file EX: ./ssOrigami.dat')
+    parser.add_argument('-i', '--input_md_file', metavar='input_md_file', type=str,
+                        help='Path to oxDNA input file EX: ./input')
+    parser.add_argument('-c', '--coding_sequence', metavar='output_name', type=str,
+                        help='Path to .txt or .fasta file coding sequence EX: ./coding_seq.txt')
+    parser.add_argument('-o', '--output_name', metavar='output_name', type=str,
+                        help='name of the Default: ./fatcat_tmalign_homology_search.csv')
+    parser.add_argument('--force_overwrite', action='store_true',
+                        help='Force overwrite of existing results if they exist.')
+
+    args = parser.parse_args()
+    return args, parser
 
 def collect_args():
     """
@@ -39,6 +69,17 @@ def collect_args():
         strucutre_file = Path(args.strucutre_file)
         if not strucutre_file.exists():
             raise ValueError(f'Provided strucutre_file path {strucutre_file} does not exist')
+        assert strucutre_file.suffix == '.dat', f"strucutre_file must be a .dat file and it currently {strucutre_file.suffix}"
+
+
+    if not args.topology_file:
+        parser.print_help()
+        raise ValueError('Please provide a topology_file using the -t or --topology_file flag')
+    else:
+        topology_file = Path(args.topology_file)
+        if not topology_file.exists():
+            raise ValueError(f'Provided topology_file path {topology_file} does not exist')
+        assert topology_file.suffix == '.top', "topology_file must be a .top file"
 
     if not args.input_md_file:
         parser.print_help()
@@ -55,6 +96,14 @@ def collect_args():
         coding_sequence = Path(args.coding_sequence)
         if not coding_sequence.exists():
             raise ValueError(f'Provided coding_sequence path {coding_sequence} does not exist')
+        if coding_sequence.suffix not in ['.txt', '.fasta']:
+            raise ValueError('coding_sequence must be a .txt or .fasta file')
+        with open(coding_sequence, 'r') as f:
+            lines = f.readlines()
+            if coding_sequence.suffix == '.fasta':
+                assert len(lines) == 2, "fasta file must have 2 lines"
+            else:
+                assert len(lines) == 1, "txt file must have 1 line"
 
     if not args.output_name:
         output_name = 'coding_sequence_embbeded_strucutre'
@@ -65,26 +114,103 @@ def collect_args():
         if (Path(f'{output_name}.dat').exists()) or (Path(f'{output_name}.top').exists()):
             raise ValueError(f'Output file {output_name} already exists.\
                 Please use the --force_overwrite flag to overwrite the file.')
-    return strucutre_file, input_md_file, coding_sequence, output_name
-
-def get_structure():
-    pass
+    return strucutre_file, topology_file, input_md_file, coding_sequence, output_name
 
 
-def get_hblist():
-    pass
+def parse_coding_sequence(coding_sequence):
+    if coding_sequence.suffix == '.fasta':
+        with open(coding_sequence, 'r') as f:
+            lines = f.readlines()
+            coding_seq_str = lines[1]
+    elif coding_sequence.suffix == '.txt':
+        with open(coding_sequence, 'r') as f:
+            coding_seq_str = f.readline()[0]
+
+    unique_bases = set(coding_seq_str)
+    possible_bases = set('ATCGUatcgu')
+
+    assert unique_bases.issubset(possible_bases), "coding_sequence must only contain A, T, C, G, or U"
+    coding_seq_str = coding_seq_str.upper()
+    return coding_seq_str
 
 
-def get_halfs():
-    pass
+def get_structure(strucutre_file, topology_file):
+    strucutre = dna.load_dna_structure(topology_file, strucutre_file)
+    return strucutre
 
 
-def get_idx_of_coding_complement():
-    pass
+def get_hblist(strucutre_file, topology_file, input_md_file):
+
+    inline_code =  f"""
+from oxDNA_analysis_tools.output_bonds import output_bonds
+from oxDNA_analysis_tools.UTILS.RyeReader import describe
+top_info, traj_info = describe("{topology_file.as_posix()}", "{strucutre_file.as_posix()}")
+output_bonds(traj_info, top_info, "{input_md_file.as_posix()}")
+    """
+
+    result = subprocess.run([sys.executable, '-c', inline_code], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, text=True)
+
+    stdout_output = result.stdout
+
+    potential_information = [info for info in stdout_output.split('\n') if "#" not in info]
+    parsed_potentials = [info.strip().split(' ') for info in potential_information]
+    parsed_potentials = np.array([info for info in parsed_potentials if len(info) == 11], dtype=float)
+
+    headers = stdout_output.split('\n')[0].split(' ')
+    potential_names = headers[:-3]
+    potential_names[0] = potential_names[0][1:]
+    potential_names[-1] = potential_names[-1][:-1]
+    
+    df = pd.DataFrame(parsed_potentials, columns=potential_names)
+    hb_series = df[df['HB'] < -0.1]
+    hb_id_1 = hb_series['id1']
+    hb_id_2 = hb_series['id2']
+    
+    return hb_id_1, hb_id_2
 
 
-def find_where_coding_binds_coding():
-    pass
+def get_halfs(strucutre):
+    n_bases = strucutre.get_num_bases()
+    half = int(np.floor(n_bases/2))
+    
+    indexes_1 = np.arange(half)
+    indexes_2 = np.arange(half, n_bases)
+
+    return half, indexes_1, indexes_2
+
+
+def make_pair_map(hb_id_1, hb_id_2):
+    pair_map = {}
+    for i, j in zip(hb_id_1, hb_id_2):
+        pair_map[i] = j
+        pair_map[j] = i
+    return pair_map
+
+
+def get_idx_of_coding_complement(indexes_1, indexes_2, pair_map, coding_seq_str):
+    assert len(coding_seq_str) <= len(indexes_1), "coding sequence is longer than half of the structure"
+    
+    second_half_reversed = indexes_2[::-1]
+    coding_indexes = second_half_reversed[:len(coding_seq_str)]
+    coding_complement = [pair_map[i] for i in coding_indexes]
+    
+    return coding_indexes, coding_complement
+
+
+def find_where_coding_binds_coding(coding_indexes, coding_complement, pair_map):
+    coding_binds_coding_indexes = []
+    for code in coding_indexes:
+        if code in coding_complement:
+            coding_binds_coding_indexes.append((code, pair_map[code]))
+    return coding_binds_coding_indexes
+
+
+def get_index_to_seq_map(strucutre):
+    index_to_seq_map = {}
+    for idx in range(strucutre.get_num_bases()):
+        base = strucutre.get_base(idx)
+        index_to_seq_map[idx] = base.base[0]
+    return index_to_seq_map
 
 
 def mutate_strucutre():
--- tests/test_structring_strand_design.py
+++ tests/test_structring_strand_design.py
@@ -1,2 +1,201 @@
 import pytest
 import strand_design. structring_strand_design as ssd
+from pathlib import Path
+import shutil
+import os
+import ipy_oxdna.dna_structure as dna
+
+class TestStructringStrandDesign:
+    """
+    Test driven development of the structring strand design algorithm
+    """
+    def create_dir_with_all_files(self, path):
+        file_dir = path / 'test_files'
+        file_dir.mkdir()
+
+        ex_structure_file = Path('./strucutres/science_melting/nanobase_files/last_conf_MD2.dat')
+        shutil.copy2(ex_structure_file, file_dir)
+        
+        ex_topology_file = Path('./strucutres/science_melting/nanobase_files/ssRNA_science2.top')
+        shutil.copy2(ex_topology_file, file_dir)
+
+        ex_input_md_file = Path('./strucutres/science_melting/nanobase_files/inputMD')
+        shutil.copy2(ex_input_md_file, file_dir)
+
+        ex_coding_sequence = Path('./strucutres/science_melting/nanobase_files/coding_seq.txt')
+        shutil.copy2(ex_coding_sequence, file_dir)
+        
+        ex_sequence_dependant_parameters = Path('./strucutres/science_melting/nanobase_files/rna_sequence_dependent_parameters.txt')
+        shutil.copy2(ex_sequence_dependant_parameters, file_dir)
+        
+        # with open(f'{file_dir}/inputMD', 'r') as f:
+        #     lines = f.readlines()
+        
+        # new_line = f'seq_dep_file = {file_dir}/rna_sequence_dependent_parameters.txt\n'    
+        # with open(f'{file_dir}/inputMD', 'w') as f:
+        #     for line in lines:
+        #         if "seq_dep_file = rna_sequence_dependent_parameters.txt" in line:
+        #             f.write(new_line)
+        #         else:
+        #             f.write(line)
+
+        return file_dir
+
+
+    @pytest.fixture
+    def dir_all_files(self, tmp_path):
+        file_dir = self.create_dir_with_all_files(tmp_path)
+        return file_dir
+
+
+    @pytest.fixture
+    def dir_fasta_coding_seq(self, tmp_path):
+        file_dir = self.create_dir_with_all_files(tmp_path)
+        with open(file_dir / 'coding_seq.txt', 'r') as f:
+            lines = f.readlines()
+            new_line = '>coding_sequence\n'
+            with open(file_dir / 'coding_seq.fasta', 'w') as f:
+                f.write(new_line)
+                f.writelines(lines)
+        os.remove(file_dir / 'coding_seq.txt')
+
+        return file_dir
+
+
+    @pytest.fixture
+    def dir_no_struc(self, tmp_path):
+        file_dir = self.create_dir_with_all_files(tmp_path)
+        os.remove(file_dir / 'last_conf_MD2.dat')
+        return file_dir
+
+
+    @pytest.fixture
+    def dir_no_input_md(self, tmp_path):
+        file_dir = self.create_dir_with_all_files(tmp_path)
+        os.remove(file_dir / 'inputMD')
+        return file_dir
+
+
+    @pytest.fixture
+    def dir_no_coding_sequence(self, tmp_path):
+        file_dir = self.create_dir_with_all_files(tmp_path)
+        os.remove(file_dir / 'coding_seq.txt')
+        return file_dir
+    
+    
+    def set_args(self, my_dir:Path):
+        test_args = [
+            'structring_strand_design.py',
+            '-s', f'{my_dir.as_posix()}/last_conf_MD2.dat',
+            '-t', f'{my_dir.as_posix()}/ssRNA_science2.top',
+            '-i', f'{my_dir.as_posix()}/inputMD',
+            '-c', f'{my_dir.as_posix()}/coding_seq.txt'
+            ]
+        return test_args
+    
+    
+    def test_collect_args_all_files(self, dir_all_files, monkeypatch):
+        test_args = self.set_args(dir_all_files)
+        monkeypatch.setattr('sys.argv',test_args)
+        
+        strucutre_file,  topology_file, input_md_file, coding_sequence, output_name = ssd.collect_args()
+        assert strucutre_file.exists()
+        assert topology_file.exists()
+        assert input_md_file.exists()
+        assert coding_sequence.exists()
+        assert output_name == 'coding_sequence_embbeded_strucutre'
+        
+        
+    def test_collect_args_no_struc(self, dir_no_struc, monkeypatch):
+        test_args = self.set_args(dir_no_struc)
+        monkeypatch.setattr('sys.argv',test_args)
+        
+        try:
+            ssd.collect_args()
+        except ValueError as e:
+            assert str(e) == f'Provided strucutre_file path {dir_no_struc}/last_conf_MD2.dat does not exist'
+            
+            
+    def test_collect_args_no_input_md(self, dir_no_input_md, monkeypatch):
+        test_args = self.set_args(dir_no_input_md)
+        monkeypatch.setattr('sys.argv',test_args)
+        
+        failed = False
+        try:
+            ssd.collect_args()
+        except ValueError as e:
+            assert str(e) == f'Provided input_md_file path {dir_no_input_md}/inputMD does not exist'
+            failed = True
+        
+        assert failed is True  
+
+
+    def test_collect_args_no_coding_sequence(self, dir_no_coding_sequence, monkeypatch):
+        test_args = self.set_args(dir_no_coding_sequence)
+        monkeypatch.setattr('sys.argv',test_args)
+        
+        try:
+            strucutre_file,  topology_file, input_md_file, coding_sequence, output_name = ssd.collect_args()
+        except ValueError as e:
+            assert str(e) == f'Provided coding_sequence path {dir_no_coding_sequence}/coding_seq.txt does not exist'
+
+
+    def test_parse_coding_sequence_txt(self, dir_all_files, monkeypatch):
+        test_args = self.set_args(dir_all_files)
+        monkeypatch.setattr('sys.argv',test_args)
+        strucutre_file, topology_file, input_md_file, coding_sequence, output_name = ssd.collect_args()
+        assert coding_sequence.suffix == '.txt'
+        
+        coding_seq_str = ssd.parse_coding_sequence(coding_sequence)
+        possible_bases = set('ATCGU')
+        assert set(coding_seq_str).issubset(possible_bases)
+        assert type(coding_seq_str) == str
+        
+
+    def test_parse_coding_sequence_fasta(self, dir_fasta_coding_seq, monkeypatch):
+        test_args = self.set_args(dir_fasta_coding_seq)
+        test_args = [arg if 'coding_seq.txt' not in arg else f'{dir_fasta_coding_seq}/coding_seq.fasta' for arg in test_args]
+        monkeypatch.setattr('sys.argv',test_args)
+        strucutre_file, topology_file, input_md_file, coding_sequence, output_name = ssd.collect_args()
+        assert coding_sequence.suffix == '.fasta'
+        
+        coding_seq_str = ssd.parse_coding_sequence(coding_sequence)
+        possible_bases = set('ATCGU')
+        assert set(coding_seq_str).issubset(possible_bases)
+        assert type(coding_seq_str) == str
+
+
+    def test_get_strucutre(self, dir_all_files, monkeypatch):
+        test_args = self.set_args(dir_all_files)
+        monkeypatch.setattr('sys.argv',test_args)
+        strucutre_file, topology_file, input_md_file, coding_sequence, output_name = ssd.collect_args()
+        
+        strucutre = ssd.get_structure(strucutre_file, topology_file)
+        assert isinstance(strucutre, dna.DNAStructure)
+        
+        
+    def test_get_hb_list(self, dir_all_files, monkeypatch):
+        test_args = self.set_args(dir_all_files)
+        monkeypatch.setattr('sys.argv',test_args)
+        strucutre_file, topology_file, input_md_file, coding_sequence, output_name = ssd.collect_args()
+        cwd = os.getcwd()
+        os.chdir(dir_all_files)
+        hb_id_1, hb_id_2 = ssd.get_hblist(strucutre_file, topology_file, input_md_file)
+        os.chdir(cwd)
+        assert len(hb_id_1) == len(hb_id_2)
+        
+        # I want to be able to make sure that no id in hb_id_1 is in hb_id_2
+        assert len(set(hb_id_1).intersection(set(hb_id_2))) == 0
+        
+        
+  
+    def test_main(self, dir_all_files, monkeypatch):
+        test_args = self.set_args(dir_all_files)
+        monkeypatch.setattr('sys.argv',test_args)
+        cwd = os.getcwd()
+        os.chdir(dir_all_files)
+        out = ssd.main()
+        os.chdir(cwd)
+        assert out == 0
+        
+        
